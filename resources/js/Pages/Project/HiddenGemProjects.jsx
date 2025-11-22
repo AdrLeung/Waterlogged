a7c695c6c0c47eed15ad39fb9dfef33cb324817b
Simon Mattern <simonmattern03@gmail.com>
2025-11-22 14:01:15 -0800

    Hidden Gems basic UI

--- resources/js/Pages/Project/HiddenGemProjects.jsx
+++ resources/js/Pages/Project/HiddenGemProjects.jsx
@@ -5,15 +5,21 @@ export default function HiddenGemProjects({ projects, superUsers }) {
 
     return (
         <AppLayout>
-            <h1>These are some projects that needs some extra support!</h1>
-            {projects.map((project) => (
-                <div key={project.projectID}>
-                    <p>{project.name}</p>
-                    <p>{project.description}</p>
-                    <p>{project.obs_amount}</p>
-                    <br />
-                </div>
-            ))}
-        </AppLayout>
+        <div className="flex items-start justify-center min-h-screen p-6">
+            <div className="w-full max-w-2xl space-y-10">
+
+        <div className="gap-4 p-8 space-y-4 bg-white border rounded-lg">
+        <h1 className="p-2 text-2xl font-bold">These are some projects that needs extra support!</h1>
+        {projects.map((project) => (
+            <div className="flex justify-between p-6 space-x-3 space-y-2 border rounded-lg ">
+                <p className="font-semibold">{project.name}</p>
+                <p>{project.description}</p>
+                <p>Observations: {project.obs_amount}</p>
+            </div>
+        ))}
+        </div>
+        </div>
+        </div>
+    </AppLayout>
     );
 }
