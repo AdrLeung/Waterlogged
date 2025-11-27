63c9a55ce7bb25c7a0cda6b9befc8d373dca7bad
Simon Mattern <simonmattern03@gmail.com>
2025-11-27 12:49:03 -0800

    Basic Project View UI implementation

--- resources/js/Pages/Project/ShowProject.jsx
+++ resources/js/Pages/Project/ShowProject.jsx
@@ -1,4 +1,6 @@
 import AppLayout from "@/Layouts/AppLayout";
+import { Head, router, usePage } from "@inertiajs/react";
+import { Button } from "@/Components/ui/button";
 
 export default function ShowProject({ name, description, observations }) {
     console.log(name);
@@ -7,13 +9,31 @@ export default function ShowProject({ name, description, observations }) {
 
     return (
         <AppLayout>
-            <p>name: {name}</p>
-            <p>description: {description}</p>
+            <div className="flex items-start justify-center min-h-screen p-6">
+                <div className="justify-between w-full max-w-md p-8 space-y-3 text-center bg-white rounded-2xl">
+            <p className="text-2xl font-extrabold">Project Name: {name}</p>
+            <p>Description: {description}</p>
             <ul>
                 {observations.map((obs) => (
                     <li key={obs.observationID}>{obs.notes}</li>
                 ))}
             </ul>
+
+            <div className="grid grid-cols-1 gap-4">
+                        <Button
+                            onClick={() => router.get(route("groupChat.index"))}
+                        >
+                            See Groupchats
+                        </Button>
+
+                        <Button
+                            onClick={() => router.get(route("project.index"))}
+                        >
+                            Back to Projects
+                        </Button>
+                    </div>
+            </div>
+            </div>
         </AppLayout>
     );
 }
